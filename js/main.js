ae8f485d5e9fc07d34cf371f7c6438022e9f33d0
Baphomet <zloostpvp@gmail.com>
2025-05-20 18:24:36 -0300

    finalização do site, adicionado: cursos, agendamento, footer e alguns detalhes

--- js/main.js
+++ js/main.js
@@ -21,3 +21,61 @@ document.addEventListener('DOMContentLoaded', () => {
     },
   });
 });
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   new Swiper('.swiper-cursos', {
+//     wrapperClass:   'swiper-wrapper-cursos',
+//     slideClass:     'swiper-slide-cursos',
+//     loop:           true,
+
+//       freeMode: {
+//       enabled: true,
+//       sticky:  true,
+//       momentum: true,      
+//     },
+
+//     slidesPerView:  6,
+//     spaceBetween:   10,
+//     speed:          9000,
+//     autoplay: {
+//       delay:             10,
+//       disableOnInteraction: false,
+//       waitForTransition: false,
+//     },
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+//     breakpoints: {
+//       320:  { slidesPerView: 2, spaceBetween: 10 },
+//       640:  { slidesPerView: 4, spaceBetween: 15 },
+//       1024: { slidesPerView: 6, spaceBetween: 20 },
+//     },
+//   });
+// });
+
+const inputs = document.querySelectorAll(".formulario input");
+
+inputs.forEach(input => {
+    input.addEventListener("blur", function () {
+        if (input.value.trim() === "") {
+            input.classList.add("erro");
+        } else {
+            input.classList.remove("erro");
+        }
+    });
+});
+
+const input2 = document.querySelectorAll(".f-newsletter input");
+
+input2.forEach(input => {
+    input.addEventListener("blur", function () {
+        if (input.value.trim() === "") {
+            input.classList.add("borda");
+        } else {
+            input.classList.remove("borda");
+        }
+    });
+});
+
+
